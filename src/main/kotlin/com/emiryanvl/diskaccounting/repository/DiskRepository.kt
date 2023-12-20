09ee53f60419fd5d91de2616c04b33ac36a7fea2
2oublePeace <69040128+2oublePeace@users.noreply.github.com>
2023-12-20 19:45:03 +0400

    Delete src directory

--- src/main/kotlin/com/emiryanvl/diskaccounting/repository/DiskRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.emiryanvl.diskaccounting.repository
-
-import com.emiryanvl.diskaccounting.entity.DiskEntity
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface DiskRepository : JpaRepository<DiskEntity, Long>
