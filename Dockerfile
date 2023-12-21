0f048aa5430a9fe156437abbc677b70d81acb958
2oublePeace <vova.emiryan@yandex.ru>
2023-12-21 04:05:13 +0400

    Сделан docker build

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,23 @@
-FROM 2oublepeace/disk-accounting-boomq
+FROM gradle:jdk17 as builder
+
+WORKDIR /app
+
+COPY . .
+
+RUN gradle bootJar
+
+FROM eclipse-temurin:17-jre-alpine
+
+WORKDIR /app
+
+ENV SPRING_PROFILES_ACTIVE dev
+
+ENV DATABASE_USERNAME postgres
+
+ENV DATABASE_PASSWORD jrmjFwJojvYHqdRU
+
+ENV DATABASE_URL jdbc:postgresql://db.faealilgdzehkthqhxwf.supabase.co:5432/postgres?user=postgres&password=jrmjFwJojvYHqdRU
+
+COPY --from=builder /app/build/libs/*.jar ./disk-accounting.jar
+
+CMD ["java", "-jar", "disk-accounting.jar"]
--- src/test/kotlin/com/emiryanvl/diskaccounting/DiskAccountingApplicationTests.kt
+++ src/test/kotlin/com/emiryanvl/diskaccounting/DiskAccountingApplicationTests.kt
@@ -1,24 +1,62 @@
 package com.emiryanvl.diskaccounting
 
+import com.emiryanvl.diskaccounting.dto.request.DiskRequest
+import com.emiryanvl.diskaccounting.service.impl.DiskServiceImpl
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class DiskAccountingApplicationTests {
 
+    @Autowired
+    private lateinit var diskService: DiskServiceImpl
+
     @Test
-    fun test1() {
-        assertEquals(5, 5)
+    fun createDiskItemTest() {
+        val request = DiskRequest("Фильм 1", false)
+        val response = diskService.create(request)
+        assertEquals(request.title, response.title)
+        assertEquals(request.isRented, response.isRented)
     }
 
     @Test
-    fun test2() {
-        assertEquals(5, 5)
+    fun getAllDiskItemsTest() {
+        diskService.create(DiskRequest("Фильм 1", false))
+        diskService.create(DiskRequest("Фильм 2", true))
+        diskService.create(DiskRequest("Фильм 3", false))
+        diskService.create(DiskRequest("Фильм 4", true))
+        diskService.create(DiskRequest("Фильм 5", true))
+
+        val response = diskService.getAll()
+        assertTrue(response.isNotEmpty())
     }
 
     @Test
-    fun test3() {
-        assertEquals(5, 5)
+    fun getCurrentItemTest() {
+        diskService.create(DiskRequest("Фильм 1", false))
+        val response = diskService.get(1)
+        assertEquals(response.isRented, false)
+    }
+
+    @Test
+    fun updateCurrentItemTest() {
+        diskService.create(DiskRequest("Фильм 1", false))
+        val request = DiskRequest("Мультфильм 1", false)
+        val response = diskService.update(1, request)
+        assertEquals(request.title, response.title)
+        assertEquals(request.isRented, response.isRented)
+    }
+
+    @Test
+    fun deleteCurrentItemTest() {
+        diskService.create(DiskRequest("Фильм 1", false))
+        var disks = diskService.getAll()
+        assertTrue(disks.isNotEmpty())
+        diskService.delete(1)
+        disks = diskService.getAll()
+        assertTrue(disks.isEmpty())
     }
 }
